7fd144475d90200adf2f9dee927b7e01602e5bd6
stitou-nada <nadastito1087@gmail.com>
2021-12-20 12:00:49 +0100

    add

--- projet v1/calcul.js
+++ projet v1/calcul.js
@@ -4,17 +4,5 @@ var b
 //traitement
 function calculer() 
 { 
-     a = document.getElementById("output").value 
-     b = a
-    document.getElementById("output").value = b 
-} 
-//fonction qui affiche la valeur
-function afficher(val) 
-{ 
-    document.getElementById("output").value+=val 
-} 
-//fonction qui efface l'écran 
-function effacer() 
-{ 
-    document.getElementById("output").value = "" 
+   
 } 
