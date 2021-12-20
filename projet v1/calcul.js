1e04290822443d9b220b976ff0a7c82acba5d033
stitou-nada <nadastito1087@gmail.com>
2021-12-20 11:51:50 +0100

    add

---
+++ projet v1/calcul.js
@@ -0,0 +1,20 @@
+// declaration des variable
+var a 
+var b 
+//traitement
+function calculer() 
+{ 
+     a = document.getElementById("output").value 
+     b = eval(a)
+    document.getElementById("output").value = b 
+} 
+//fonction qui affiche la valeur
+function afficher(val) 
+{ 
+    document.getElementById("output").value+=val 
+} 
+//fonction qui efface l'écran 
+function effacer() 
+{ 
+    document.getElementById("output").value = "" 
+} 
