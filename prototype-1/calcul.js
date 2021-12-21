37379eeba1690c1f22b5381be5d62e56d2690c0a
stitou-nada <nadastito1087@gmail.com>
2021-12-21 12:28:10 +0100

    add

--- prototype-1/calcul.js
+++ prototype-1/calcul.js
@@ -1,3 +1,86 @@
+// Variables
+var x,y,solution,operation;
+var xString, yString;
 
+
+
+// Métier
+function Calculer(x,y,operation){
+    var _solution = undefined;
+    switch (operation) {
+        case '+':
+            _solution = x + y;
+            break;
+        case '-':
+            _solution = x-y;
+                break;
+        default:
+            break;
+    }
+    return _solution;
+}
+
+
+// traitement
+function ClickNumber(number){
+    if(x == undefined){
+        if(xString == undefined) xString = '';
+        xString += number;
+    } 
+    else {
+        if(yString == undefined) yString = '';
+        yString += number;
+    } 
+
+    Afficher();
+}
+
+function Afficher(number){
+
+    var afficheur = document.getElementById("afficheur");
+    afficheur.value = "";
+    
+    if(x != undefined && y != undefined && operation != undefined){
+
+        afficheur.value = number;
+    }else{
+        if(xString != undefined)
+        afficheur.value += xString 
+        if(operation != undefined)
+            afficheur.value += operation     
+        if(yString != undefined)
+            afficheur.value += yString 
+
+        }
+ 
+   
+}
+
+
+function Operation(operationParam){
+    if(operation == undefined){
+        operation = operationParam;
+        x = parseFloat(xString);
+        Afficher();
+    }
+}
+
+function Egale(){
+    x = parseFloat(xString);
+    y = parseFloat(yString);
+    solution = Calculer(x,y,operation);
+    Afficher(solution);
+}
+
+function Init(){
+    x = undefined;
+    y = undefined;
+    xString = undefined;
+    yString = undefined;
+    operation = undefined;
+    var afficheur = document.getElementById("afficheur");
+    
+    afficheur.value = "";
+}
 
   
